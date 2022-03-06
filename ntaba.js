7da4e486d3d4c0653484f22d235c46d3dc0864fa
princentaba4@gmail.com <princentaba4@gmail.com>
2022-03-06 19:01:02 +0300

    11th commit added days to the ids created

--- ntaba.js
+++ ntaba.js
@@ -1,3 +1,30 @@
 let ntaba=document.getElementById("DAY")
 let gender1=document.getElementById("Male")
-let gender2=document.getElementById("Female")
+let gender2=document.querySelector("#Female")
+let akan=document.getElementById("AKAN")
+let highway=document.getElementById("locator")
+let maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+let femaleNames=[]
+console.log(highway);
+highway.addEventListener("submit",(y)=>{
+    y.preventDefault()
+    kamal()
+})
+function kamal(){
+    let day1=ntaba.value
+let gender3=gender1.value
+let gender4=gender2.value
+    if(gender3==="male"&day1==="Sunday"){
+        akan.innerText=`${maleNames[0]}`
+    }else if(gender3==="male"&day1==="Monday"){
+        akan.innerText=`${maleNames[1]}`
+    }else if(gender3==="male"&day1==="Tuesday"){
+        akan.innerText=`${maleNames[2]}`
+    }else if(gender3==="male"&day1==="Wednesday"){
+        akan.innerText=`${maleNames[3]}`    
+    else{
+        akan.innerText="invalid input"
+
+    }
+}
+
