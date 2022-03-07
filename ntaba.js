76cab02e0c7ce4bb8aadfd60792a8bee3e5f6c53
princentaba4@gmail.com <princentaba4@gmail.com>
2022-03-07 09:18:20 +0300

    14th commit linked the days to the female names

--- ntaba.js
+++ ntaba.js
@@ -14,20 +14,38 @@ function kamal(){
     let day1=ntaba.value
 let gender3=gender1.value
 let gender4=gender2.value
+
     if(gender3==="male"&day1==="Sunday"){
         akan.innerText=`${maleNames[0]}`
-    }else if(gender3==="male"&day1==="Monday"){
+        }else if(gender3==="male"&day1==="Monday"){
         akan.innerText=`${maleNames[1]}`
-    }else if(gender3==="male"&day1==="Tuesday"){
+            }else if(gender3==="male"&day1==="Tuesday"){
         akan.innerText=`${maleNames[2]}`
-    }else if(gender3==="male"&day1==="Wednesday"){
+                }else if(gender3==="male"&day1==="Wednesday"){
         akan.innerText=`${maleNames[3]}`  
-    }else if(gender3==="male"&day1==="Thursday"){
+                    }else if(gender3==="male"&day1==="Thursday"){
         akan.innerText=`${maleNames[4]}` 
-    }else if(gender3==="male"&day1==="Friday"){
+                        }else if(gender3==="male"&day1==="Friday"){
         akan.innerText=`${maleNames[5]}`    
-    }else if(gender3==="male"&day1==="Saturday"){
-        akan.innerText=`${maleNames[6]}`         
-    }else{
+                            }else if(gender3==="male"&day1==="Saturday"){
+        akan.innerText=`${maleNames[6]}`
+
+        if(gender4==="female"&day1==="Sunday"){
+        akan.innerText=`${femaleNames[0]}`
+    }else if(gender4==="female"&day1==="Monday"){
+    akan.innerText=`${femaleNames[1]}`
+        }else if(gender4==="female"&day1==="Tuesday"){
+    akan.innerText=`${femaleNames[2]}`
+            }else if(gender4==="female"&day1==="Wednesday"){
+    akan.innerText=`${femaleNames[3]}`  
+                }else if(gender4==="female"&day1==="Thursday"){
+    akan.innerText=`${femaleNames[4]}` 
+                    }else if(gender4==="female"&day1==="Friday"){
+    akan.innerText=`${femaleNames[5]}`    
+                        }else if(gender4==="female"&day1==="Saturday"){
+    akan.innerText=`${femaleNames[6]}`
+    }
+
+    else{
         akan.innerText="invalid input"
-    }}
+    }}}
